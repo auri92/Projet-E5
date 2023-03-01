3f36f2a66474d55d66f9d773e7b254d41f58e078
pc fix <houzeyu7@gmail.com>
2023-03-01 01:18:56 +0100

    add back button and button to login

--- app/src/main/java/com/example/projet_e5/LoginMainActivity.java
+++ app/src/main/java/com/example/projet_e5/LoginMainActivity.java
@@ -1,14 +1,29 @@
 package com.example.projet_e5;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LoginMainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+
+        back_Home();
+    }
+
+    protected void back_Home(){
+        ImageButton button_home = findViewById(R.id.buttonhome);
+
+        button_home.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setClass(LoginMainActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/projet_e5/MainActivity.java
+++ app/src/main/java/com/example/projet_e5/MainActivity.java
@@ -1,30 +1,28 @@
 package com.example.projet_e5;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private Button button_login = null;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_login = (Button)findViewById(R.id.button_pageLogin);
-        button_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,LoginMainActivity.class);
-                startActivity(intent);
-            }
-        });
+        go_Login();
 
+    }
+
+    protected void go_Login(){
+        Button button_login = findViewById(R.id.button_pageLogin);
+        button_login.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this,LoginMainActivity.class);
+            startActivity(intent);
+        });
     }
 }
