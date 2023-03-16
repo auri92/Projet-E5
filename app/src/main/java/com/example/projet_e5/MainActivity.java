f6b0c63dd8f492ce83011f2f5339f23bc853c375
HZY552 <houzeyu7@gmail.com>
2023-03-16 09:40:10 +0100

    update

--- app/src/main/java/com/example/projet_e5/MainActivity.java
+++ app/src/main/java/com/example/projet_e5/MainActivity.java
@@ -18,7 +18,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
